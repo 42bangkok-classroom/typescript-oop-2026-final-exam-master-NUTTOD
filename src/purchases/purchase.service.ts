5132d7a15e679bcbea405e62de15096ce5e8af25
NUTTOD <nuttod88@gmail.com>
2026-03-25 12:13:21 +0700

    p02_1

--- src/purchases/purchase.service.ts
+++ src/purchases/purchase.service.ts
@@ -7,7 +7,7 @@ import * as path from 'path';
 @Injectable()
 export class PurchaseService {
   findAll(): Purchase<PurchaseItem> {
-    const filepath = path.join(process.cwd(), 'data', 'products.json');
+    const filepath = path.join(process.cwd(), 'data', 'purchases.json');
     const data = fs.readFileSync(filepath, 'utf-8');
     const showData = JSON.parse(data) as PurchaseItem;
     return {
