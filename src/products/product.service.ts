8e248ae99692b7737f6d25a5363fbc17e7d574b7
NUTTOD <nuttod88@gmail.com>
2026-03-25 10:47:37 +0700

    p01_2

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -8,12 +8,11 @@ export class ProductService {
   findAll(): Product<object> {
     const filepath = path.join(process.cwd(), 'data', 'products.json');
     const data = fs.readFileSync(filepath, 'utf-8');
+    const showData = JSON.parse(data) as Product<object>
     return {
         success: true,
-        data: [
-            JSON.parse(data)
-        ],
+        data: showData,
         message: 'Fetched products successfully',
-    } as Product<object>;
+    };
   }
 }
