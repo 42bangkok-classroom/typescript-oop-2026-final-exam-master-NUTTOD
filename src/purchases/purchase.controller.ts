5267e6ef5ad7fe7bcb4069817def312f8777265c
NUTTOD <nuttod88@gmail.com>
2026-03-25 12:14:10 +0700

    p02_2

--- src/purchases/purchase.controller.ts
+++ src/purchases/purchase.controller.ts
@@ -3,7 +3,6 @@ import type { Purchase } from './purchase.interface';
 import { PurchaseService } from './purchase.service';
 import { PurchaseItem } from './purchase.interface';
 
-
 @Controller()
 export class PurchaseController {
   constructor(private readonly productService: PurchaseService) {}
--- src/purchases/purchase.interface.ts
+++ src/purchases/purchase.interface.ts
@@ -1,17 +1,19 @@
 export interface PurchaseItem {
-    id: number;
-    customerName: string;
-    purchaseDate: string;
-    items: [{
-        productId: number;
-        quantity: number;
-        price:number;
-    }];
-    totalPrice: number
+  id: number;
+  customerName: string;
+  purchaseDate: string;
+  items: [
+    {
+      productId: number;
+      quantity: number;
+      price: number;
+    },
+  ];
+  totalPrice: number;
 }
 
 export interface Purchase<PurchaseItem> {
-    success: boolean;
-    data: PurchaseItem | null;
-    message: string;
+  success: boolean;
+  data: PurchaseItem | null;
+  message: string;
 }
