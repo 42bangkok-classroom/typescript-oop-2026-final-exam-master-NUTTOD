a049b711d76b7c8edeaf22d3a48d762a20b0b13e
NUTTOD <nuttod88@gmail.com>
2026-03-25 10:21:25 +0700

    p00_4

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  status(): ApiResponse<object> {
+  getHello(): ApiResponse<object> {
     return this.appService.statusCheck();
   }
 }
